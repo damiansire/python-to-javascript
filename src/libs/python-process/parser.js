30317f18b72c69122a880c52489b5f6beca1cd53
Damian Sire <damiansire@hotmail.com>
2023-06-26 21:40:24 -0300

    feat: Added support for multiple variables with the same name

--- src/libs/python-process/parser.js
+++ src/libs/python-process/parser.js
@@ -1,17 +1,72 @@
 const { isVariableAssignment } = require("./detectElement.js");
 
-const processLine = (line) => {
-  if (isVariableAssignment(line)) {
-    return "const " + line;
+const getVariableIdentifier = (line) => {
+  //Evita los espacios, ejemplo numero1 = 5, numero1    =    5, '    numero2    =    10    '
+  const patron = /^\s*([a-zA-Z_][a-zA-Z0-9_]*)\s*=/;
+  const coincidencia = line.match(patron);
+  if (coincidencia && coincidencia.length > 1) {
+    return coincidencia[1];
   }
-  console.log("test");
+  return null;
+};
+
+const processLine = (line, variablesCount, variableHistory) => {
+  if (isVariableAssignment(line)) {
+    const identifier = getVariableIdentifier(line);
+    if (variableHistory.has(identifier)) {
+      return line;
+    }
+    if (variablesCount.getOccurrences(identifier) > 1) {
+      start = variableHistory.has(identifier) ? "" : "let";
+    } else {
+      start = "const";
+    }
+    variableHistory.add(identifier);
+    return `${start} ${line}`;
+  }
   return `${line} : No contemplada`;
 };
 
+class KeyCounter {
+  constructor() {
+    this.occurrences = {};
+  }
+
+  addOcurrence(key) {
+    if (key in this.occurrences) {
+      this.occurrences[key]++;
+    } else {
+      this.occurrences[key] = 1;
+    }
+  }
+
+  getOccurrences(key) {
+    return this.occurrences[key] || 0;
+  }
+}
+
+function countVariables(lines) {
+  const variablesCount = new KeyCounter();
+  for (line of lines) {
+    if (isVariableAssignment(line)) {
+      const identifier = getVariableIdentifier(line);
+      variablesCount.addOcurrence(identifier);
+    }
+  }
+
+  return variablesCount;
+}
+
 const parserContent = (content) => {
   const fileLines = content.split(/\r?\n/);
-  let processLines = fileLines.map(processLine);
-  let parsedContent = processLines.join("\n");
+  const processedLines = [];
+  const variablesCount = countVariables(fileLines);
+  const variableHistory = new Set();
+  for (line of fileLines) {
+    const processedLine = processLine(line, variablesCount, variableHistory);
+    processedLines.push(processedLine);
+  }
+  let parsedContent = processedLines.join("\n");
   return parsedContent;
 };
 
--- src/libs/python-process/parser.test.js
+++ src/libs/python-process/parser.test.js
@@ -1,10 +1,20 @@
 const { parserContent } = require("./parser");
 describe("parserContent", () => {
-  test("should return parsed variable if you have 2 diferent variables", () => {
+  test("should declare variables with const if not repeated", () => {
     const input = `name = "damian"
 surname = "sire"`;
     const expected = `const name = "damian"
 const surname = "sire"`;
+    const output = parserContent(input);
+    expect(output).toEqual(expected);
+  });
+  test("should declare the variables with let if they are repeated", () => {
+    const input = `name = "damian"
+surname = "sire"
+name = "pedro"`;
+    const expected = `let name = "damian"
+const surname = "sire"
+name = "pedro"`;
     const output = parserContent(input);
     expect(output).toEqual(expected);
   });
